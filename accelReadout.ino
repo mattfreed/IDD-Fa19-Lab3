2e5ca4426244a828b56428a100f8b0f59cf24b57
Matt Freed <mwf58@cornell.edu>
2019-09-20 13:15:16 -0400

    LCD and RGB Readout for accelerometer

---
+++ accelReadout.ino
@@ -0,0 +1,131 @@
+// Basic demo for accelerometer readings from Adafruit LIS3DH
+
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_LIS3DH.h>
+#include <Adafruit_Sensor.h>
+
+// include the library code:
+#include <LiquidCrystal.h>
+
+// Used for software SPI
+#define LIS3DH_CLK 13
+#define LIS3DH_MISO 12
+#define LIS3DH_MOSI 11
+// Used for hardware & software SPI
+#define LIS3DH_CS 10
+
+// software SPI
+//Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS, LIS3DH_MOSI, LIS3DH_MISO, LIS3DH_CLK);
+// hardware SPI
+//Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS);
+// I2C
+Adafruit_LIS3DH lis = Adafruit_LIS3DH();
+
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+int redPin = 6;
+int greenPin = 10;
+int bluePin = 9;
+
+void setup(void) {
+// set up the LCD's number of columns and rows:
+lcd.begin(16, 2);
+
+pinMode(redPin, OUTPUT);
+pinMode(greenPin, OUTPUT);
+pinMode(bluePin, OUTPUT);  
+
+#ifndef ESP8266
+  while (!Serial);     // will pause Zero, Leonardo, etc until serial console opens
+#endif
+
+  Serial.begin(9600);
+  Serial.println("LIS3DH test!");
+  
+  if (! lis.begin(0x18)) {   // change this to 0x19 for alternative i2c address
+    Serial.println("Couldnt start");
+    while (1);
+  }
+  Serial.println("LIS3DH found!");
+  
+  lis.setRange(LIS3DH_RANGE_4_G);   // 2, 4, 8 or 16 G!
+  
+  Serial.print("Range = "); Serial.print(2 << lis.getRange());  
+  Serial.println("G");
+}
+
+void loop() {
+  lcd.clear();
+  lis.read();      // get X Y and Z data at once
+  // Then print out the raw data
+  Serial.print("X:  "); Serial.print(lis.x); 
+  Serial.print("  \tY:  "); Serial.print(lis.y); 
+  Serial.print("  \tZ:  "); Serial.print(lis.z); 
+ 
+    
+  
+  /* Or....get a new sensor event, normalized */ 
+  sensors_event_t event; 
+  lis.getEvent(&event);
+  
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("\t\tX: "); Serial.print(event.acceleration.x);
+  Serial.print(" \tY: "); Serial.print(event.acceleration.y); 
+  Serial.print(" \tZ: "); Serial.print(event.acceleration.z); 
+  Serial.println(" m/s^2 ");
+
+  Serial.println();
+//print x values
+  lcd.setCursor(1, 0);
+  lcd.print("X: ");
+  lcd.setCursor(4, 0);
+  lcd.print(event.acceleration.x);
+
+//print y values
+  lcd.setCursor(9, 0);
+  lcd.print("Y: ");
+  lcd.setCursor(12, 0);
+  lcd.print(event.acceleration.y);
+
+ //print z values
+  lcd.setCursor(1, 1);
+  lcd.print("Z: ");
+  lcd.setCursor(4, 1);
+  lcd.print(event.acceleration.z);
+
+  //colors
+  int iX = 0;
+  int iY = 0;
+  int iZ = 0;
+
+  if(event.acceleration.x >= 0){
+    iX = 255;
+  }
+  if(event.acceleration.y >= 0){
+    iY = 255;
+  }
+  if(event.acceleration.z >= 0){
+    iZ = 255;
+  }
+    
+setColor(iX, iY, iZ);  // colors change as acc changes
+ 
+  delay(200); 
+}
+
+void setColor(int red, int green, int blue)
+{
+  #ifdef COMMON_ANODE
+    red = 255 - red;
+    green = 255 - green;
+    blue = 255 - blue;
+  #endif
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);  
+}
